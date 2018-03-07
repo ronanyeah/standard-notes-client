245b4b914590d88686a1b9e5475bd858a85ae32f
ronan <ronan_mccabe@protonmail.com>
2018-03-07 04:16:54 +0000

    set up scaffold

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { resolve } = require("path");
+
+const { DEBUG, NODE_ENV } = process.env;
+
+const publicFolder = resolve("./public");
+
+module.exports = {
+  mode: NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/index.js",
+  output: {
+    path: publicFolder,
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: publicFolder,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          { loader: "elm-hot-loader" },
+          {
+            loader: "elm-webpack-loader",
+            options: {
+              cwd: __dirname,
+              debug: DEBUG === "true",
+              warn: NODE_ENV === "development"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new CopyWebpackPlugin(["static"])]
+};
